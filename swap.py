e8d381613051934943a00119a698538f04b40e52
mounish9894978881 <82440929+mounish9894978881@users.noreply.github.com>
2021-04-13 15:36:45 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the first number a:"))
+b=int(input("Enter the second number b:"))    
+a,b=b,a
+print("After swapping")
+print("The value of a is ",a)
+print("The value of b is ",b)     
